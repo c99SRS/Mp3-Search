88ea9fdcedf94e6322b96cf8b35694b227c4a992
c99SRS <smruti.work@gmail.com>
2017-03-22 03:39:51 +0530

    modifying file

--- find_mp3.sh
+++ find_mp3.sh
@@ -4,6 +4,31 @@ echo "Finding your mp3 files ............"
 echo " "
 echo "Counting no of  mp3 files. Wait......"
 sudo find /home/ -iname "*.mp3" -print  | wc -l
-
+echo ""
 
 sudo find /home/ -iname "*.mp3" -print       # it will search in home directory for all mp3 files
+echo " "
+echo -n "Would you like to group in one directory? Type [y/n] "
+read response
+
+# if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+#     echo "Hello  successful " 
+# else
+#     echo "Very bad " 
+# fi
+
+
+# case "$response" in ([yY] [eE] [ss] | [yY])
+#      echo "Hello  successful " ;; 
+#      *)
+	
+#      echo "Very bad " ;
+
+# esac
+
+case $response in [Yy][Ee][Ss]|[Yy]) 
+      echo " Moving  your files............. "
+      
+      echo "Hello  successful " ;; *) 
+    echo " very bad ." ;; 
+esac
