1960bd52c74f07c44ebae5c9521ba9a0cd2a33b6
c99SRS <smruti.work@gmail.com>
2017-03-23 23:37:34 +0530

    automation of folder creation

--- find_mp3.sh
+++ find_mp3.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#! Author - Smruti Ranjan
-#! Date : 22/03/2017
+#Created on 23-0-2017
+#Author = Smruti Ranjan
+#Version 1.0
+#START
 
 
+DIR = /home/smruti/MP3
 echo "Finding your mp3 files ............"
 echo " "
 echo "Counting no of  mp3 files. Wait......"
@@ -18,8 +21,17 @@ read response
 
 case $response in [Yy][Ee][Ss]|[Yy]) 
       echo " Moving  your files............. "
-      mkdir /home/smruti/MP3
-     sudo find / -iname "*.mp3" -exec mv {} /home/smruti/MP3 \;
+      if [ ! -d $DIR ]
+     then
+        echo " directory " $DIR " does not exist. creating directory ........";
+        mkdir  $DIR
+        sudo find / -iname "*.mp3" -exec mv {} /home/smruti/MP3 \;
+      else
+        echo "directory " $DIR " exists. Copying files .........";
+        sudo find / -iname "*.mp3" -exec mv {} /home/smruti/MP3 \;
+      fi
+     
       echo "Hello  successful " ;; *) 
     echo " very bad . You Should organize  things ....... :-0 " ;; 
 esac
+
